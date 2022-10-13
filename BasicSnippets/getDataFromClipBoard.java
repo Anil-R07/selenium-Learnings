ee465195bac15d3264a58f4e861d25a793ce118a
Shrijith <104303253+shrijithps25@users.noreply.github.com>
2022-10-13 10:33:56 +0530

    getDataFromClipboard
    Code for getting the copied data from the clipboard

--- BasicSnippets/getDataFromClipBoard.java
+++ BasicSnippets/getDataFromClipBoard.java
@@ -1,5 +1,12 @@
 //The snippet allows you to copy data from clipboard, can be used to get data from textBox when getAttribute() or getText() isn't working
 
+/**
+*
+* Get the copied data from the clipboard
+*
+*/
+
+
 public void getDataFromClipBoard(){
 	Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 	Transferable contents = clipboard.getContents(null);
